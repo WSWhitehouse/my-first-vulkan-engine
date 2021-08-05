2a8d85447b44cbd9f791514a9b90c01ffa00ccbd
WSWhitehouse <williamswhitehouse@gmail.com>
2021-08-05 13:10:37 +0100

    Updated FileSystem to use std::string rather than char* and malloc

--- mfve/src/core/FileSystem.cpp
+++ mfve/src/core/FileSystem.cpp
@@ -15,31 +15,39 @@ namespace MFVE
     MFVE_LOG_INFO("Initialising File System");
 
     // Base Path
-    int dirname_length = 0;
-    int length         = wai_getExecutablePath(nullptr, 0, nullptr);
-    char* path         = (char*)malloc(length + 1);
-    wai_getExecutablePath(path, length, nullptr);
-    path[length] = '\0';
-
-    m_basePath = fs::path(path).remove_filename();
-    free(path);
-    MFVE_LOG_INFO("    Base Path:   " + m_basePath.string());
-
-    // Asset Path
-    m_assetPath = m_basePath / "assets";
-
-    if (!std::filesystem::exists(m_assetPath))
     {
-      MFVE_LOG_FATAL(
-        "    ABORTING! Unable to locate Assets. Have you tried running 'Generate-Assets'?");
-      return;
+      int length = wai_getExecutablePath(nullptr, 0, nullptr);
+      std::string path;
+      path.resize(length);
+
+      wai_getExecutablePath(path.data(), path.capacity(), nullptr);
+
+      if (path.empty())
+      {
+        MFVE_LOG_FATAL("ABORTING! Unable to locate Base Path. Base path is empty!");
+        return;
+      }
+
+      m_basePath = fs::path(path).remove_filename();
+
+      MFVE_LOG_INFO("    Base Path:   " + m_basePath.string());
     }
 
-    MFVE_LOG_INFO("    Assets Path: " + m_assetPath.string());
+    // Asset Path
+    {
+      m_assetPath = m_basePath / "assets";
+
+      if (!std::filesystem::exists(m_assetPath))
+      {
+        MFVE_LOG_FATAL(
+          "ABORTING! Unable to locate Assets. Have you tried running 'Generate-Assets'?");
+        return;
+      }
+
+      MFVE_LOG_INFO("    Assets Path: " + m_assetPath.string());
+    }
 
     MFVE_LOG_INFO("Finished Initialising File System");
     MFVE_LOG_INFO("------------------------------");
-
-    return;
   }
 } // namespace MFVE
