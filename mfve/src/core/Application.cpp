25712661d940810b14c0db657f84c7f382da75c0
WSWhitehouse <williamswhitehouse@gmail.com>
2021-08-30 22:54:29 +0100

    Formatted Code

--- mfve/src/core/Application.cpp
+++ mfve/src/core/Application.cpp
@@ -23,8 +23,9 @@ namespace MFVE
     /* Window */
     WindowProperties windowProps{};
 
-    // Creating a GLFW Window for now - in future automatically decide which window is best on each platform
-    auto _window = new WindowGLFW(windowProps); 
+    // Creating a GLFW Window for now - in future automatically
+    // decide which window is best on each platform
+    auto _window = new WindowGLFW(windowProps);
 
     /* Vulkan */
     m_renderer.CreateRenderer(GetAppProperties(), _window, nullptr);
