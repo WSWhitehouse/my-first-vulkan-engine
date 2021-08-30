58d118cc554396dcfa69a997abb26b1079b8443f
WSWhitehouse <williamswhitehouse@gmail.com>
2021-08-30 23:14:50 +0100

    Fix Linker warnings on Windows
    No longer linking MFVE with OS libraries - which were redefining macros causing linker errors

--- mfve/engine-premake.lua
+++ mfve/engine-premake.lua
@@ -93,20 +93,6 @@ project "MFVE"
         defines { "_CRT_SECURE_NO_WARNINGS", "_ALLOW_ITERATOR_DEBUG_LEVEL_MISMATCH" }
 
         links {
-            --# OS Links #--
-            "kernel32.lib",
-            "user32.lib",
-            "gdi32.lib",
-            "winspool.lib",
-            "comdlg32.lib",
-            "advapi32.lib",
-            "shell32.lib",
-            "ole32.lib",
-            "oleaut32.lib",
-            "uuid.lib",
-            "odbc32.lib",
-            "odbccp32.lib",
-
             --# SPIR-V & ShaderC #--
             "shaderc_combined.lib",
             --"shaderc_shared.lib",
